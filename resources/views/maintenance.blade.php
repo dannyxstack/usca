dd21c97ab059aa7cd90d7dde5c362cabafa4ff8a
dannyxstack <danielxu1100@gmail.com>
2026-05-19 18:56:54 +0800

    update maintenance

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <title>Website Under Maintenance</title>
-    <style>body { text-align: center; padding: 150px; } h1 { font-size: 50px; }</style>
+    <style>body { text-align: center; padding: 150px; } h1 { font-size: 40px; }</style>
 </head>
 <body>
     <h1>This website is currently under review and construction</h1>
-    <p>Unity Skills College of Health</p>
+    <p>© 2026 Unity Skills College of Health</p>
 </body>
 </html>
